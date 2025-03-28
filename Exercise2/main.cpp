96821e0720ca7b74400e361666c2176a33982dbf
ElenaFaraci <s311482@studenti.polito.it>
2025-03-28 10:51:59 +0100

    Update main.cpp

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
 int main()
 {
-    return 0;
+	std::ifstream fstr("data.txt");
+	std::ofstream oFile("result.txt");
+	
+	oFile << "N Mean" << "\n";
+
+	if(fstr.fail())
+	{
+		std::cerr << "File not found" << std::endl;
+		return 1;
+	}
+	
+	double val;
+	double sum = 0;
+	double mean = 0;
+	unsigned int n = 1;
+	double val2;
+	while(fstr >> val)
+	{
+		val2 = 0.75  * val - 1.75;
+		sum += val2;
+		mean = sum / n;
+		oFile << n << " " << std::setprecision(16) << std::scientific << mean << "\n";
+		n++;
+	}
+
+	
+	return 0;
 }
